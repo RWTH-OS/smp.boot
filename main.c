da8d8651a4672587be419286cec14c226a7006cf
Georg Wassen <wassen@lfbs.rwth-aachen.de>
2011-08-29 08:02:40 +0000

    clean up the crappy API from that tutorial, this code was based on. No warnings any more.

--- main.c
+++ main.c
@@ -1,74 +1,8 @@
 #include <system.h>
 #include <multiboot.h>
 
-/* You will need to code these up yourself!  */
-unsigned char *memcpy(unsigned char *dest, const unsigned char *src, int count)
+void print_multiboot_info(multiboot_info_t *mbi) 
 {
-    /* Add code here to copy 'count' bytes of data from 'src' to
-    *  'dest', finally return 'dest' */
-    const char *sp = (const char *)src;
-    char *dp = (char *)dest;
-    for(; count != 0; count--) *dp++ = *sp++;
-    return dest;
-}
-
-unsigned char *memset(unsigned char *dest, unsigned char val, int count)
-{
-    /* Add code here to set 'count' bytes in 'dest' to 'val'.
-    *  Again, return 'dest' */
-    char *temp = (char *)dest;
-    for( ; count != 0; count--) *temp++ = val;
-    return dest;
-}
-
-unsigned short *memsetw(unsigned short *dest, unsigned short val, int count)
-{
-    /* Same as above, but this time, we're working with a 16-bit
-    *  'val' and dest pointer. Your code can be an exact copy of
-    *  the above, provided that your local variables if any, are
-    *  unsigned short */
-    unsigned short *temp = (unsigned short *)dest;
-    for( ; count != 0; count--) *temp++ = val;
-    return dest;
-}
-
-int strlen(const char *str)
-{
-    /* This loops through character array 'str', returning how
-    *  many characters it needs to check before it finds a 0.
-    *  In simple words, it returns the length in bytes of a string */
-    int retval;
-    for(retval = 0; *str != '\0'; str++) retval++;
-    return retval;
-}
-
-/* We will use this later on for reading from the I/O ports to get data
-*  from devices such as the keyboard. We are using what is called
-*  'inline assembly' in these routines to actually do the work */
-unsigned char inportb (unsigned short _port)
-{
-    unsigned char rv;
-    __asm__ __volatile__ ("inb %1, %0" : "=a" (rv) : "dN" (_port));
-    return rv;
-}
-
-/* We will use this to write to I/O ports to send bytes to devices. This
-*  will be used in the next tutorial for changing the textmode cursor
-*  position. Again, we use some inline assembly for the stuff that simply
-*  cannot be done in C */
-void outportb (unsigned short _port, unsigned char _data)
-{
-    __asm__ __volatile__ ("outb %1, %0" : : "dN" (_port), "a" (_data));
-}
-
-/* This is a very simple main() function. All it does is sit in an
-*  infinite loop. This will be like our 'idle' loop */
-void main(multiboot_info_t *mbi)
-{
-    init_video();
-
-    puts("my kernel!\n");
-
     printf("Multiboot-Flags: 0x%x\n", mbi->flags);
 
     if (mbi->flags & (1<<0)) {
@@ -102,19 +36,28 @@ void main(multiboot_info_t *mbi)
 
     if (mbi->flags & (1<<6)) {
         printf("flags[6] - mmap_length: %d  mmap_addr: 0x%x\n", mbi->mmap_length, mbi->mmap_addr);
-        multiboot_memory_map_t* p = mbi->mmap_addr;
-        for ( ; p < mbi->mmap_addr+mbi->mmap_length; p = ((void*)p + p->size + 4)) {
+        multiboot_memory_map_t* p = (multiboot_memory_map_t*)mbi->mmap_addr;
+        for ( ; p < (multiboot_memory_map_t*)(mbi->mmap_addr+mbi->mmap_length); p = ((void*)p + p->size + 4)) {
             printf("  mmap[0x%x] - base_addr: 0x%x %x  length: 0x%x %x   type: %d\n",
                    p, p->addr, p->len, p->type);
         }
     }
+}
 
+
+void main(multiboot_info_t *mbi)
+{
+    init_video();
+
+    puts("my kernel is running in main now...\n");
+
+    print_multiboot_info(mbi);
 
     printf("The end.\n");
 
     /* ...and leave this loop in. There is an endless loop in
      *  'start.asm' also, if you accidentally delete this next line 
      */
-    for (;;);
+    while (1) {};
 }
 		
--- system.h
+++ system.h
@@ -3,19 +3,19 @@
 
 
 /* MAIN.C */
-extern unsigned char *memcpy(unsigned char *dest, const unsigned char *src, int count);
-extern unsigned char *memset(unsigned char *dest, unsigned char val, int count);
-extern unsigned short *memsetw(unsigned short *dest, unsigned short val, int count);
-extern int strlen(const char *str);
-extern unsigned char inportb (unsigned short _port);
-extern void outportb (unsigned short _port, unsigned char _data);
+void *memcpy(void *dest, const void *src, int count);
+void *memset(void *dest, int val, int count);
+unsigned short *memsetw(unsigned short *dest, unsigned short val, int count);
+int strlen(const char *str);
+unsigned char inportb (unsigned short _port);
+void outportb (unsigned short _port, unsigned char _data);
 
 /* SCRN.H */
-extern void cls();
-extern void putch(unsigned char c);
-extern void puts(unsigned char *str);
-extern void settextcolor(unsigned char forecolor, unsigned char backcolor);
-extern void init_video();
+void cls();
+void putch(char c);
+void puts(char *str);
+void settextcolor(unsigned char forecolor, unsigned char backcolor);
+void init_video();
 void printf (const char *format, ...);
 
 #endif
