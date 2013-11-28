ce0413a5de5c9d1a72abbd7d66f07edf16a59ba3
Georg Wassen <georg.wassen@googlemail.com>
2013-11-28 21:54:40 +0100

    define license in all files

--- boot32.c
+++ boot32.c
@@ -1,8 +1,50 @@
 /*
- * This is 32 bit C code for help during the boot sequence.
- * It is used for the 64 bit kernel but should be suited for the 32 bit kernel, too.
- * This is called from startXX.asm in REAL MODE (physical addresses)
+ * =====================================================================================
+ *
+ *       Filename:  boot32.c
+ *
+ *    Description:  This is 32 bit C code for help during the boot sequence. It is used 
+ *                  for the 64 bit kernel but should be suited for the 32 bit kernel, too.
+ *                  This is called from startXX.asm in REAL MODE (physical addresses)
+ *
+ *        Version:  1.0
+ *        Created:  02.09.2011 14:04:00
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Georg Wassen (gw) (wassen@lfbs.rwth-aachen.de), 
+ *        Company:  Lehrstuhl für Betriebssysteme (Chair for Operating Systems)
+ *                  RWTH Aachen University
+ *
+ * Copyright (c) 2011, Georg Wassen, RWTH Aachen University
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *    * Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *    * Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *    * Neither the name of the University nor the names of its contributors
+ *      may be used to endorse or promote products derived from this
+ *      software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * =====================================================================================
  */
+
+
 
 #include "multiboot_struct.h"
 #include "bda_struct.h"
